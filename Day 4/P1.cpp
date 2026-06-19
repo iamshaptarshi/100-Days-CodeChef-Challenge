d3647120a6adddf50d9a96a2490e4035efac8e6d
mdshap <notunmdshaptarshi@gmail.com>
2026-06-20 02:40:35 +0600

    Day 4 Completed

---
+++ Day 4/P1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int X;
+    cin >> X;
+    cout << (X <= 60 ? "YES" : "NO");
+    return 0;
+}
---
+++ Day 4/tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+    cout << (B >= 3 * A ? "Rain" : "Dry");
+    return 0;
+}
